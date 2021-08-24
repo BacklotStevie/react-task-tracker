e34e7acdf090dad994d1f7972eeda0c448af1663
Steven Sarria <ssarr012@fiu.edu>
2021-08-24 15:38:50 -0400

    Fixed button onclick

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,6 @@
 import PropTypes from 'prop-types'
 
 const Button = ({color, text, onClick}) => {
-    const onClick = () => {
-    }
     return <button  onClick={onClick}
     style={{backgroundColor: color }}className='btn'>{text}</button>
 }
